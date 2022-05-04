bb3e3764b01f7244d0c99260c1870dbfd7320547
= <=>
2022-05-04 03:02:53 -0500

    configuration for webpack jquery and bootstrap installation - no more scrript reference

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const webpack = require("webpack");
 // create the main configuration object within our file
 module.exports = { 
   entry: './assets/js/script.js',  //the relative path to the client's code
@@ -6,6 +7,12 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'main.bundle.js'
   },
+  plugins:[
+    new webpack.ProvidePlugin({
+      $: "jquery",
+      jQuery: "jquery"
+    }),
+  ],
   mode: 'development'  // By default, webpack wants to run in production mode //webpack will minify our code for us automatically
-
+  
 };
