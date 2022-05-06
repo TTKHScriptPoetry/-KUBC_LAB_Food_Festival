2f772031d79c5d06ae9a32253b13381242656652
= <=>
2022-05-06 02:14:50 -0500

    pwa manifes config

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const webpack = require("webpack");
 const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
+const WebpackPwaManifest = require("webpack-pwa-manifest");
 
 // create the main configuration object within our file
 
@@ -49,10 +50,30 @@ module.exports = {
     }),
     new BundleAnalyzerPlugin({
       analyzerMode: "static", // the report outputs to an HTML file in the dist folder
+    }),
+    new WebpackPwaManifest({
+      name: "Food Event",
+      short_name: "Foodies",
+      description: "An app that allows you to view upcoming food events.",
+      start_url: "index.html", // relative to the location of the manifest file.
+      background_color: "#01579b",
+      theme_color: "#ffffff",
+      fingerprints: false, // should NOT generate unique fingerprints manifest.lhge325d.jso
+      inject: false, //  manifest.json is NOT added to our HTML; will manually hardcode the path to the manifest.json in our html instead.
+      icons: [{
+        src: path.resolve("assets/img/icons/icon-512x512.png"),
+        sizes: [96, 128, 192, 256, 384, 512],
+        destination: path.join("assets", "icons")
+      }]
     })
-    
+
   ],
-  mode: 'development'  // By default, webpack wants to run in production mode //webpack will minify our code for us automatically
+  mode: 'development',  // By default, webpack wants to run in production mode //webpack will minify our code for us automatically
+  devServer: {
+    static: "./dist",
+    port: 8080
+  }
+    
   // (*)
   // the bundle file for script.js will be app.bundle.js, 
   // the bundle file for events.js will be events.bundle.js, and so on,
